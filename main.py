d126e2e6c8e2ab6ac9297b36266a91523d1e0cea
J Sikes <31295952+TechRancher@users.noreply.github.com>
2023-10-02 18:25:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
     Author: Jason Sikes AKA The Tech Rancher
     Version: 1.0
     Date Coded: 10/02/2023
-    Description: This circuit reads data from the RFID RC522 and Reads the position value of the SG90 Servo and displays either Locked or Unlocked status on the SSD1306 Display along with one of two LED's will light up. Red LED for Locked status or Green LED status for Unlocked. The MC is a Raspberry Pi Pico W.
+    Description: This circuit reads data from the RFID RC522 Reads the position value of the SG90 Servo and displays either Locked or Unlocked status on the SSD1306 Display along with one of two LED's will light up. Red LED for Locked status or Green LED status for Unlocked. The MC is a Raspberry Pi Pico W.
     License: MIT License
 """
 # Import Libraries
@@ -32,19 +32,23 @@ Lock_State = 0
 
 # Create Objects
 i2c = I2C(0,scl=Pin(dsp_scl_Pin), sda=Pin(dsp_sda_Pin), freq=i2c_freq)
-dsp = SSD1306_I2C(dsp_Width, dsp_Heigh, i2c)
+dsp = SSD1306_I2C(dsp_Width, dsp_Heigh, i2c) # This is the SSD display Object
 
 servo = PWM(Pin(servo_Pin))
 servo.freq(50)
 servo.duty_u16()
 
-reader = MFRC522(sck=rfid_sck_Pin, mosi=rfid_mosi_Pin, miso=rfid_miso_Pin,cs=rfid_cs_Pin, rst=rfid_rst_Pin, spi_id=1)
+reader = MFRC522(sck=rfid_sck_Pin, mosi=rfid_mosi_Pin, miso=rfid_miso_Pin,cs=rfid_cs_Pin, rst=rfid_rst_Pin, spi_id=1) # This is the RC522 Object
 
 LED_Red = Pin(red_LED_Pin, Pin.OUT)
 LED_Green = Pin(green_LED_Pin, Pin.OUT)
 
 # Functions
 def centerText(txt):
+    """
+        This will center text on the SSD1306
+        Argument txt is the text that you want to be centered
+    """
     text = (len(txt) *8) /2
     mid = 64 - text
     return int(mid)
@@ -90,7 +94,7 @@ def LED_Status(servo_stat,redLED,greenLED):
         greenLED.on()
 
 dsp.fill(0)
-dsp.show()
+dsp.show() # This will clear anything on the SSD1306 Display
 
 try:
     while True:
@@ -105,25 +109,27 @@ try:
         dsp.text(lockStatusMsg, centerText(lockStatusMsg),16,1)
         dsp.show() # Displays the message of Read Card and Lock status
 
-        (stat, tag_type) = reader.request(reader.REQIDL)
+        (stat, tag_type) = reader.request(reader.REQIDL) # Access the address to read the data on the card
         if stat == reader.OK:
-            (stat, uid) = reader.SelectTagSN()
+            (stat, uid) = reader.SelectTagSN() # Read the data from the card to see if it matches the required card to use with RC522
             if stat == reader.OK:
                 dsp.fill(0)
                 card = int.from_bytes(bytes(uid), "little", False) # type: ignore
-                print(str(card))
+                # Takes the Hexadecimal and turns it into small "little" bytes to be used.
+                print(str(card)) # Prints to Terminal the card bytes number
                 msg1 = f"Card Id: {str(card)}"
                 dsp.text(msg,centerText(msg),0,1)
                 dsp.text(msg1,0,8,1)
-                dsp.show()
-                if str(card) == "60198054":
-                    if servo.duty_u16() == 8191:
+                dsp.show() # Displays messages to the SSD1306
+
+                if str(card) == "60198054": # Looks for this card number.
+                    if servo.duty_u16() == 8191: # servo SG90 uses 8191 to be 180 degrees and 1638 to be 0 degrees
                         setLock(True)
                         time.sleep(2)
                     else:
                         setLock(False)
                         time.sleep(2)
-                elif str(card) == "2166693146":
+                elif str(card) == "2166693146": # Change this if you want to read another card to control the servo
                     if servo.duty_u16() == 8191:
                         setLock(True)
                         time.sleep(2)
@@ -132,8 +138,8 @@ try:
                         time.sleep(2)
         time.sleep_ms(500)
 except KeyboardInterrupt:
-    dsp.fill(0)
+    dsp.fill(0) # Clears the SSD1306 display
     dsp.show()
     LED_Green.off()
     LED_Red.off()
-    soft_reset()
+    soft_reset() # Resets the Pico W MC
